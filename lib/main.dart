89bb6b2e0f3bf1f94ff399059e944fa3bba2ba33
Farkhan Azmi <ghost@Farkhans-MacBook-Pro.local>
2022-05-13 09:43:34 +0700

    update::set min size window on desktop app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:monking/ui/screens/screens.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Monking');
+    setWindowMinSize(const Size(320, 640));
+  }
   runApp(const MyApp());
 }
 
